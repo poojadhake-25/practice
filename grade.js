1a9a6f703bd91a2690a051611f7c23b5beae06a1
poojadhake-25 <56387282+poojadhake-25@users.noreply.github.com>
2019-10-14 11:13:42 +0530

    Add files via upload

---
+++ grade.js
@@ -0,0 +1,21 @@
+var avg=77;
+if(avg<60)
+{
+console.log("grade :E");
+}
+else if(avg < 70 )
+{
+ console.log("Grade :D");   
+}    
+else if(avg < 80)
+{
+    console.log("Grade :C");   
+} 
+else if(avg < 90)
+{
+    console.log("Grade :B");   
+   } 
+   else if(avg < 100)
+   {
+    console.log("Grade :A");   
+   } 
