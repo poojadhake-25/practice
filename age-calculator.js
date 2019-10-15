830b8380bb87ef7b82700445a410bb5ead232ee6
poojadhake-25 <56387282+poojadhake-25@users.noreply.github.com>
2019-10-15 17:37:46 +0530

    Delete age-calculator.js

--- age-calculator.js
+++
@@ -1,8 +0,0 @@
-function calculate_age(dob) { 
-   var diff_ms = Date.now() - dob.getTime();
-   var age_dt = new Date(diff_ms); 
- 
-   return Math.abs(age_dt.getUTCFullYear() - 1970);
-}
-
-console.log(calculate_age(new Date(1990, 10, 1)));
